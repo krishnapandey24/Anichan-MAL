3079544491fa897f451945928768ab3e6637f9c8
Krishna Pandey <krishnnapandey@gmail.com>
2022-09-21 22:30:12 +0530

    changed Currently Airing text margin

--- app/src/main/java/com/omnicoder/anichan/UI/Fragments/ScheduleFragment.java
+++ app/src/main/java/com/omnicoder/anichan/UI/Fragments/ScheduleFragment.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 
@@ -20,6 +21,7 @@ import com.omnicoder.anichan.R;
 import com.omnicoder.anichan.ViewModels.ScheduleViewModel;
 import com.omnicoder.anichan.databinding.FragmentScheduleBinding;
 
+import java.util.Calendar;
 import java.util.List;
 
 import dagger.hilt.android.AndroidEntryPoint;
@@ -42,11 +44,11 @@ public class ScheduleFragment extends Fragment {
         ScheduleViewModel viewModel = new ViewModelProvider(this).get(ScheduleViewModel.class);
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
         viewModel.fetchSchedule();
-        viewModel.getSchedule().observe(getViewLifecycleOwner(), this::initSpinners);
+        viewModel.getSchedule().observe(getViewLifecycleOwner(), schedule -> initSpinners(schedule,Calendar.getInstance().get(Calendar.DAY_OF_WEEK)));
     }
 
 
-    private void initSpinners(Schedule schedule) {
+    private void initSpinners(Schedule schedule,int day) {
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getContext(), R.array.weekdays, android.R.layout.simple_spinner_item);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.scheduleSpinner.setAdapter(arrayAdapter);
@@ -55,25 +57,25 @@ public class ScheduleFragment extends Fragment {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0:
-                        setRecyclerView(schedule.getMonday());
+                        setRecyclerView(schedule.getSunday());
                         break;
                     case 1:
-                        setRecyclerView(schedule.getTuesday());
+                        setRecyclerView(schedule.getMonday());
                         break;
                     case 2:
-                        setRecyclerView(schedule.getWednesday());
+                        setRecyclerView(schedule.getTuesday());
                         break;
                     case 3:
-                        setRecyclerView(schedule.getThursday());
+                        setRecyclerView(schedule.getWednesday());
                         break;
                     case 4:
-                        setRecyclerView(schedule.getFriday());
+                        setRecyclerView(schedule.getThursday());
                         break;
                     case 5:
-                        setRecyclerView(schedule.getSaturday());
+                        setRecyclerView(schedule.getFriday());
                         break;
                     case 6:
-                        setRecyclerView(schedule.getSunday());
+                        setRecyclerView(schedule.getSaturday());
                         break;
                     case 7:
                         setRecyclerView(schedule.getOther());
@@ -90,6 +92,7 @@ public class ScheduleFragment extends Fragment {
 
             }
         });
+        binding.scheduleSpinner.setSelection(day-1);
         binding.progressBar.setVisibility(View.GONE);
     }
 
